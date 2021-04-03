839f51bcf4c5e6a6895521c131d60ba99ec8ba5c
Robe <romoan@gmail.com>
2021-04-03 12:18:47 +0200

    Project initialized with webpack

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+   <h1>Hello World!</h1>
+  </div>
+</template>
+
+<style>
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-left: 5%;
+  margin-right: 5%;
+}
+</style>
